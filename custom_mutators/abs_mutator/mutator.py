c3d73e361f203a8cb38a07a6a49e4885cf058ae7
seviezhou <seviezhou@163.com>
2023-03-10 10:16:17 +0800

    Update mutator.py

--- custom_mutators/abs_mutator/mutator.py
+++ custom_mutators/abs_mutator/mutator.py
@@ -13,22 +13,22 @@ from diglib.helpers.z3utils import Z3
 import walk_sample
 import numpy as np
 import pickle  
+import time
 
 # trace file
 trace_file = "/tmp/trace.csv"
 
 # inference results for incremental refiment and input generation
-dinvs_file = "/tmp/dinvs.pkl"
-pos_file = "/tmp/pos.pkl"
+dinvs = {}
+pos_vars = []
+samples = []
 
-# leq_rhs = np.array()
-# leq = np.array()
-# eq_rhs = np.array()
-# eq = np.array()
+leq_rhs_list = []
+leq_list = []
+eq_rhs_list = []
+eq_list = []
 
-# import os
-os.environ["INFERRED"] = "1"
-inferred = 0
+time_count = {'dig' : 0.0, 'walk' : 0.0}
 
 def init(seed):
 
@@ -48,8 +48,8 @@ def write_to_file(X, Y, pos):
     # inv
     # pos = [1, 30, 44, 55, 123...]
     # vtrace1; I q; I r; I a; I b; I x; I y;
-    pos_vars = []
     inv = "vtrace1"
+    pos_vars.clear()
     for p in pos:
         inv += "; I x_{}".format(str(p))
         pos_vars.append("x_{}".format(str(p)))
@@ -58,9 +58,9 @@ def write_to_file(X, Y, pos):
         pos_vars.append("y_{}".format(str(i)))
     f.write(inv + "\n")
 
-    file = open(pos_file, 'wb')
-    pickle.dump(pos_vars, file)
-    file.close()
+    # file = open(pos_file, 'wb')
+    # pickle.dump(pos_vars, file)
+    # file.close()
 
     # vtrace
     # X = [[2, 3, 4, 5, 6...], [2, 3, 4, 5, 6...], [2, 3, 4, 5, 6...]]
@@ -83,7 +83,7 @@ def write_to_file(X, Y, pos):
 
     f.close()
 
-def get_coeff(invs, pos_vars):
+def get_coeff(invs, pos):
 
     eq_rhs = []
     eq = []
@@ -99,7 +99,7 @@ def get_coeff(invs, pos_vars):
                 eq_rhs.append(rhs)
 
                 colist = []
-                for var in pos_vars:
+                for var in pos:
                     if l[0] == sympy.symbols(var):
                         colist.append(1)
                     else:
@@ -118,7 +118,7 @@ def get_coeff(invs, pos_vars):
 
             colist = []
             coes = expr.lhs.as_coefficients_dict()
-            for var in pos_vars:
+            for var in pos:
                 sym = sympy.symbols(var)
                 if sym in coes:
                     colist.append(int(coes[sym]))
@@ -139,64 +139,87 @@ def runDig(X, Y, pos):
     inp = Path(trace_file)
 
     # run dig
+    start = time.time()
     try:
         dig = alg.DigTraces.mk(inp, None)
-        dinvs = dig.start(seed=round(time.time(), 2), maxdeg=None)
+        dig_invs = dig.start(seed=round(time.time(), 2), maxdeg=None)
+
+        dinvs.clear()
+        # persist invs
+        for key in dig_invs:
+            dinvs[key] = dig_invs[key]
+
+        # sample parameter
+        loc = list(dinvs.keys())[0]
+        cinvs = dinvs[loc].cinvs
+        leq_rhs, leq = get_coeff(cinvs.octs, pos_vars)
+        eq_rhs, eq = get_coeff(cinvs.eqts, pos_vars)
+        if len(eq) == 0:
+            eq_rhs.append(0)
+            eq.append([0] * len(pos_vars))
+        if len(leq) == 0:
+            leq_rhs.append(0)
+            leq.append([0] * len(pos_vars))
+
+        leq_rhs_list.clear()
+        leq_list.clear()
+        eq_rhs_list.clear()
+        eq_list.clear()
+
+        for item in leq_rhs:
+            leq_rhs_list.append(item)
+
+        for item in leq:
+            leq_list.append(item)      
+
+        for item in eq_rhs:
+            eq_rhs_list.append(item)
+
+        for item in eq:
+            eq_list.append(item)
+
+        # print(leq_rhs_list, leq_list, eq_list, eq_list)
     except:
         pass
-    file = open(dinvs_file, 'wb')
-    pickle.dump(dinvs, file)
-    file.close()
+
+    time_count['dig'] = time_count['dig'] + (time.time() - start)
 
 def mutate(buf, X, Y, pos):
     
-    file = open(dinvs_file, 'rb')
-    dinvs = pickle.load(file)
-    file.close()
-
-    file = open(pos_file, 'rb')
-    pos_vars = pickle.load(file)
-    file.close()
-
     if len(dinvs) == 0:
-        return buf
+        return buf  
 
-    # do sampling stuff
-    sample = []
-    
-    # if os.environ.get("INFERRED", "2") == "2":
-    loc = list(dinvs.keys())[0]
-    cinvs = dinvs[loc].cinvs
-    leq_rhs, leq = get_coeff(cinvs.octs, pos_vars)
-    eq_rhs, eq = get_coeff(cinvs.eqts, pos_vars)
-    if len(eq) == 0:
-        eq_rhs.append(0)
-        eq.append([0] * len(pos_vars))
-    if len(leq) == 0:
-        leq_rhs.append(0)
-        leq.append([0] * len(pos_vars))
-    leq_rhs = np.array(leq_rhs)
-    leq = np.array(leq)
-    eq_rhs = np.array(eq_rhs)
-    eq = np.array(eq)
+    # if there are not enough samples, try to enlarge
+    # every time 512
+    start = time.time()
+    if len(samples) == 0:
 
-    count = 1 # number of samples
-    samples = []
-    try:
-        samples = walk_sample.sample(eq, eq_rhs, leq, leq_rhs, 1)
-    except:
-        pass
+        leq_rhs = np.array(leq_rhs_list)
+        leq = np.array(leq_list)
+        eq_rhs = np.array(eq_rhs_list)
+        eq = np.array(eq_list)              
+        # print(leq_rhs_list, leq_list, eq_list, eq_list)
+        try:
+            walks = walk_sample.sample(eq, eq_rhs, leq, leq_rhs, 512)
+            for w in walks:
+                samples.append(w)
+        except:
+            pass
+
+    time_count['walk'] = time_count['walk'] + (time.time() - start)
 
     # samples [x1, x2, x3, x4] 2 times:
     # [[1, 1, 1, 1], [2, 2, 2, 2]], each list is a set of byte values
     if len(samples) > 0:
+        # print(samples)
+        sample = samples.pop()
         # constructing the mutated buff
         index = 0
         for loc in pos:
-            if samples[0][index] < 0 or samples[0][index] > 256:
+            if sample[index] < 0 or sample[index] > 256:
                 buf[loc] = 0
             else:
-                buf[loc] = int(samples[0][index])
+                buf[loc] = int(sample[index])
             index += 1
     else:
         pass
@@ -205,12 +228,11 @@ def mutate(buf, X, Y, pos):
 
 def fuzz(buf, add_buf, max_size, X, Y, pos):
 
-    
-    if os.environ.get("INFERRED", "2") == "2" and len(pos) > 0:
+    if len(pos) > 0:
         runDig(X, Y, pos)
 
     mutated_out = mutate(buf, X, Y, pos)
-
+    print(time_count)
     return mutated_out
 
     
